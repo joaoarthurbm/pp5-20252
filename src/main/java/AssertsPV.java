b30d3de136b6ff8f20e2b551c7b4bc16688f76fe
João Brunet <joaoarthurbm@gmail.com>
2026-02-26 20:08:03 -0300

    Remove redundant PVTree assertions from tests
    Removed multiple assertions related to PVTree and its method nivelComMaiorSoma.

--- src/main/java/AssertsPV.java
+++ src/main/java/AssertsPV.java
@@ -31,51 +31,6 @@ public class AssertsPV {
         assert pv.check(200) == 5;
         assert pv.check(3) == 5;
 
-        PVTree bst = new PVTree();
-        assert bst.nivelComMaiorSoma() == -1;
-
-        bst.add(10);
-        assert bst.nivelComMaiorSoma() == 0;
-
-        bst = new PVTree();
-        bst.add(41);
-        bst.add(20);
-        bst.add(65);
-        bst.add(11);
-        bst.add(29);
-        bst.add(50);
-        bst.add(91);
-        bst.add(32);
-        bst.add(72);
-        bst.add(99);
-        assert bst.nivelComMaiorSoma() == 3;
-
-        bst = new PVTree();
-        bst.add(15);
-        bst.add(6);
-        bst.add(23);
-        bst.add(4);
-        bst.add(7);
-        bst.add(71);
-        bst.add(5);
-        bst.add(50);
-        assert bst.nivelComMaiorSoma() == 2;
-
-        bst = new PVTree();
-        bst.add(61);
-        bst.add(400);
-        assert bst.nivelComMaiorSoma() == 1;
-
-        bst = new PVTree();
-        bst.add(56);
-        bst.add(45);
-        bst.add(82);
-        bst.add(15);
-        bst.add(72);
-        bst.add(94);
-        bst.add(10);
-        bst.add(99);
-        assert bst.nivelComMaiorSoma() == 2;
     }
 
 }
